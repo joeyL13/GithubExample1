3cd6326bba4d5784113ddcd634091894ccc04c2d
Litvin <joeylitvin@me.com>
2020-01-31 07:51:05 -0500

    cleaned up and added photos

---
+++ GithubExample1/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  GithubExample1
+//
+//  Created by Joseph Litvin (student LM) on 1/31/20.
+//  Copyright © 2020 Joseph Litvin (student LM). All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        // Get label node from scene and store it for use later
+    }
+}
