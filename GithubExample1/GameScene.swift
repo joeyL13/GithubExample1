aee4328ce83d0f5e5f4dc078037c74b74c4cdb2a
Litvin <joeylitvin@me.com>
2020-01-31 07:57:23 -0500

    committing

--- GithubExample1/GameScene.swift
+++ GithubExample1/GameScene.swift
@@ -12,7 +12,12 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5 )
         
-        // Get label node from scene and store it for use later
+        let player1 = SKSpriteNode(imageNamed: "player1")
+        player1.setScale(0.5)
+        player1.position = CGPoint.zero
+        player1.zPosition = 1.0// Get label node from scene and store it for use later
+        addChild(player1)
     }
 }
